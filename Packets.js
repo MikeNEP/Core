3cfc9679e198cc0d8b8052fe00ae2de9f514512e
Michael <157315242+MikeNEP@users.noreply.github.com>
2024-01-21 17:03:54 -0300

    Create Packets.js

---
+++ Packets.js
@@ -0,0 +1,28 @@
+(script = registerScript({
+    name: "PacketCounter",
+    authors: ["CzechHek"],
+    version: "1.0"
+})).import("Core.lib");
+
+module = {
+    category: "Render",
+    description: "Realiza Cuantos paquetes envias y recibes.",
+    onPacket: function (e) {
+        e.getPacket().class.simpleName.startsWith("C") ? sentPackets++ : receivedPackets++;
+    },
+    onUnload: function () {
+        timer.cancel();
+    },
+    onRender2D: function () {
+        mc.ingameGUI.drawCenteredString(mc.fontRendererObj, "§aSent§2§l " + sentPackets + " §apackets §7(" + maxSentPackets + ")", mc.displayWidth / 4, mc.displayHeight / 2.5, -1);
+        mc.ingameGUI.drawCenteredString(mc.fontRendererObj, "§cReceived§4§l " + receivedPackets + " §cpackets §7(" + maxReceivedPackets + ")", mc.displayWidth / 4, (mc.displayHeight / 2.5) + 8, -1);
+    }
+}
+
+sentPackets = receivedPackets = maxSentPackets = maxReceivedPackets = 0;
+
+timer = interval(1000, function () {
+    maxSentPackets = sentPackets;
+    maxReceivedPackets = receivedPackets;
+    sentPackets = receivedPackets = 0;
+});
